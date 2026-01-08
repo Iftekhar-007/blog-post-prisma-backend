c8240e609de5e206b6651fd80ec4cf60b4b5730e
Iftekhar-007 <shawonsp534@gmail.com>
2026-01-08 18:54:07 +0600

    comment routed added

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { postRoute } from "./modules/Post/post.route";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import cors from "cors";
+import { commentRoute } from "./modules/Comments/comment.route";
 
 const app = express();
 
@@ -16,7 +17,12 @@ app.use(express.json());
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
+// ! post routes
+
 app.use("/api/v1/posts", postRoute);
+
+// ! comment routes
+app.use("/api/v1/comments", commentRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
